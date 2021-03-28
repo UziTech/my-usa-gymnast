50ca1654370c4e8d8260a2e5ecf6e877b562ebdc
Tony Brix <tony@brix.ninja>
2021-03-28 13:25:39 -0500

    names checkboxes

--- src/Search.tsx
+++ src/Search.tsx
@@ -7,14 +7,15 @@ import {
 } from "./types";
 import names from "./names.json";
 
-function changeId(id: number) {
+function changeIds(ids: number[]) {
 	return () => {
-		window.location.href = `?id=${id}`;
+		window.location.href = `?id=${ids.join(",")}`;
 	};
 }
 
 export default function Search({search, name}: SearchProps): JSX.Element {
 	const [people, setPeople] = useState<peopleData[]>();
+	const [checked, setChecked] = useState<number[]>([]);
 	const { data, error, isPending, run } = useFetch<sanctionData>(
 		`https://uzitech.com/cbp/?url=https://api.myusagym.com/v2/sanctions/${search}`,
 		{headers: { accept: "application/json" }},
@@ -63,16 +64,31 @@ export default function Search({search, name}: SearchProps): JSX.Element {
 		}));
 	}
 
+	function changeCheckbox(id: number) {
+		return ({target}: {target: HTMLInputElement}) => {
+			if (!target) {
+				return;
+			}
+
+			if (target.checked) {
+				setChecked([...new Set([...checked, id])]);
+			} else {
+				setChecked(checked.filter(c => c !== id));
+			}
+		};
+	}
+
 	return (
 		<div className="names">
 			<h2>Pick an Athlete:</h2>
-			<ul className="names">
+			<ul>
 				{!people ? null : people.map(p => {
 					return (
-						<li className="names-button" key={p.name}><button onClick={changeId(p.id)}>{p.name}</button></li>
+						<li className="names-checkbox" key={p.name}><label><input type="checkbox" checked={checked.includes(p.id)} onChange={changeCheckbox(p.id)} />{p.name}</label></li>
 					);
 				})}
 			</ul>
+			<button onClick={changeIds(checked)}>Go</button>
 		</div>
 	);
 }
